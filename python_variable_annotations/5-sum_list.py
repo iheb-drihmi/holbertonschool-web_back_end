b81164b46c2c06cd897491c45a1fad1f6cb2693c
iheb-drihmi <ihebdrihmi2002@gmail.com>
2023-08-30 00:59:50 +0100

    tastks

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+a type-annotated function
+"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Returns the sum 
+    """
+
+    return sum(input_list)
---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+a type-annotated function sum_mixed_list
+"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    Sums a mixed list 
+    """
+
+    return sum(mxd_lst)
