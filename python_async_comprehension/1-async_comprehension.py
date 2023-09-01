16139c80d3e9735f63b3182c5fafe7f6cedadfe8
iheb-drihmi <ihebdrihmi2002@gmail.com>
2023-09-01 17:09:17 +0100

    tasks

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-The coroutine will collect 10 random 
+The coroutine will collect 10 random
 """
 
 
@@ -11,6 +11,6 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     '''
-    The coroutine will collect 10 random 
+    The coroutine will collect 10 random
     '''
     return [value async for value in async_generator()]
