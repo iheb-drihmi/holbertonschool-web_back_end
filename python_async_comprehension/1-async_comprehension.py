da7157681bce307857b355e9dedb63975da3e10f
iheb-drihmi <ihebdrihmi2002@gmail.com>
2023-09-01 16:50:52 +0100

    tasks

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+The coroutine will collect 10 random 
+"""
+
+
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    '''
+    The coroutine will collect 10 random 
+    '''
+    return [value async for value in async_generator()]
