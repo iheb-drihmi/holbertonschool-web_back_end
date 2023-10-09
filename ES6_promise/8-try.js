6dc0df115831cecc96fc80ff8d8152365d81653f
iheb-drihmi <ihebdrihmi2002@gmail.com>
2023-10-09 21:53:41 +0100

    tasks

---
+++ ES6_promise/8-try.js
@@ -0,0 +1,12 @@
+export default function divideFunction(numerator, denominator) {
+    if (denominator === 0) {
+      throw new Error('cannot divide by 0');
+    }
+  
+    try {
+      return (numerator / denominator);
+    } catch (e) {
+      console.error(e);
+      return 0;
+    }
+  }
